0ca6a38a6131d43d591306650b9210246bd95568
Hugh Kolias <hughkolias@gmail.com>
2015-07-23 00:22:21 -0400

    streams live tweets from Toronto Neighbourhood (U of T Campus to Yonge and St. Clair)

--- twitterPI.rb
+++ twitterPI.rb
@@ -1,18 +1,22 @@
 require 'rubygems' 
 require 'twitter'
+require 'chartkick'
 
-client = Twitter::REST::Client.new do |config|
+rest_client = Twitter::REST::Client.new do |config|
  config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
   config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
   config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
   config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
 end
 
-# client = Twitter::Streaming::Client.new do |config|
-#   config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
-#   config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
-#   config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
-#   config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
-# end
+stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
+  config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
+  config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
+  config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
+end
 
-print client.user.name
+stream_client.filter(locations: "-79.399837,43.657540,-79.381129,43.679445") do |tweet|
+   puts "#{tweet.user.name} says: #{tweet.text}"
+
+end
