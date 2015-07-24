a1446c67263fc8ee6ba33cd6c67944fad6ef0907
Hugh Kolias <hughkolias@gmail.com>
2015-07-24 13:31:32 -0400

    adding ability to update user's twitter account with input

--- twitterPI.rb
+++ twitterPI.rb
@@ -2,11 +2,23 @@ require 'rubygems'
 require 'twitter'
 require 'chartkick'
 
-$rest_client = Twitter::REST::Client.new do |config|
- config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
-  config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
-  config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
-  config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
+class UpdateStatus
+
+  @@rest_client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
+    config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
+    config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
+    config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
+  end
+
+  def initialize(tweet_text)
+    @tweet_text = tweet_text
+  end
+
+  def update_twitter
+    @@rest_client.update(@tweet_text)
+  end
+
 end
 
 class TwitterStream
@@ -44,9 +56,15 @@ class TwitterStream
 
 end
 
-puts "Type in what word you would like to filter?"
-filter_name = gets.chomp
-puts "filtering tweets using the word #{filter_name}"
+# puts "Type in what word you would like to filter?"
+# filter_name = gets.chomp
+# puts "filtering tweets using the word #{filter_name}"
 
-Toronto_filter = TwitterStream.new(filter_name)
-Toronto_filter.stream
+# Toronto_filter = TwitterStream.new(filter_name)
+# Toronto_filter.stream
+
+puts "Type in what you would like to tweet:"
+tweet_text = gets.chomp
+puts "updating your twitter account with the following tweet: #{tweet_text}"
+hugh = UpdateStatus.new(tweet_text)
+hugh.update_twitter
