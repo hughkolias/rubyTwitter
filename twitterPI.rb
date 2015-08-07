ac66fbec8398674db7b76b395f15dc4e1f728937
Hugh Kolias <hughkolias@gmail.com>
2015-08-06 21:15:31 -0400

    lights LED only if LED ON is said in tweet

--- twitterPI.rb
+++ twitterPI.rb
@@ -72,7 +72,7 @@ class TwitterLight
 
   def userstream
     @@stream_client.user do |object|
-      if object.is_a?(Twitter::Tweet)
+      if object.is_a?(Twitter::Tweet) && tweet.text.downcase.include?('led on')
         light_the_led
       end
     end
