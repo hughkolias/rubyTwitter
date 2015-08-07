8f74386409d84d77284d67c5c4aded5389f4f7de
Hugh Kolias <hughkolias@gmail.com>
2015-08-06 21:04:36 -0400

    create led light on user tweet

--- twitterPI.rb
+++ twitterPI.rb
@@ -1,6 +1,7 @@
 require 'rubygems' 
 require 'twitter'
 require 'chartkick'
+require 'pi_piper'
 
 class UpdateStatus
 
@@ -37,7 +38,7 @@ class TwitterStream
   end
   
   def stream
-    @@stream_client.filter(locations: "-79.399837,43.657540,-79.381129,43.679445") do |tweet|
+    @@stream_client.filter(locations: "-119.642558,49.805922,-119.619384,49.819492") do |tweet|
       if filter_is_true(tweet)
         puts "#{tweet.user.name} says: #{tweet.text}"
         print_counter
@@ -56,6 +57,38 @@ class TwitterStream
 
 end
 
+class TwitterLight
+
+  def initialize
+    @@stream_client = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = "0c8kLnzvboMTbaafm8TzbmlpE"
+      config.consumer_secret     = "bk7ObfR0LJcklusCFfD1t4myVtsdHqEWcuO4p822yqXaYudwXj"
+      config.access_token        = "356556195-CdcA6ec8ih4KpLFGyRftwryJu7ZfIYUypso2QFrb"
+      config.access_token_secret = "gQu0EgHOpzvCq9Ve5KE6afO8uHoJnutlRBf0L4acWMsLs"
+    end
+
+    pin = PiPiper::Pin.new( :pin => 17, :direction => :out )
+  end
+
+  def userstream
+    @@stream_client.user do |object|
+      if object.is_a?(Twitter::Tweet)
+        light_the_led
+      end
+    end
+  end
+
+  def light_the_led
+    pin.on
+    sleep 10
+    pin.off
+  end
+
+end
+
+bob = TwitterLight.new
+bob.userstream
+
 # puts "Type in what word you would like to filter?"
 # filter_name = gets.chomp
 # puts "filtering tweets using the word #{filter_name}"
@@ -63,8 +96,8 @@ end
 # Toronto_filter = TwitterStream.new(filter_name)
 # Toronto_filter.stream
 
-puts "Type in what you would like to tweet:"
-tweet_text = gets.chomp
-puts "updating your twitter account with the following tweet: #{tweet_text}"
-hugh = UpdateStatus.new(tweet_text)
-hugh.update_twitter
+#puts "Type in what you would like to tweet:"
+#tweet_text = gets.chomp
+#puts "updating your twitter account with the following tweet: #{tweet_text}"
+#hugh = UpdateStatus.new(tweet_text)
+#hugh.update_twitter
